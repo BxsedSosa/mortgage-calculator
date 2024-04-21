7951277e84ac20d2a9936999f9c47879a41552b0
BxsedSosa <donovanxabad@gmail.com>
2024-04-20 21:52:32 -0700

    Deleted the temp code I had from last version

--- main.py
+++ main.py
@@ -48,34 +48,3 @@ def main():
 language = 'en'
 
 main()
-def temp():
-    print(prompt(MSG[language]['welcome']))
-        
-    print(prompt(MSG[language]['loan-amount']))
-    loan_amount = input()
-
-    while valid_number(loan_amount):
-        print(prompt(MSG[language]['error-amount']))
-        loan_amount = input()
-
-    print(prompt(MSG[language]['loan-apr']))
-    loan_apr = input()
-
-    while valid_number(loan_apr):
-        print(prompt(MSG[language]['error-apr']))
-        loan_apr = input()
-
-    print(prompt(MSG[language]['loan-duration']))
-    loan_duration = input()
-
-    while valid_number(loan_duration):
-        print(prompt(MSG[language]['error-duration']))
-        loan_duration = input()
-
-    MONTHLY_INTEREST_RATE = (float(loan_apr) * .01) / 12
-    MONTHLY_DURATION = float(loan_duration) * 12
-
-    MONTHLY_PAYMENT = float(loan_amount) * (MONTHLY_INTEREST_RATE / (1 - (1 + MONTHLY_INTEREST_RATE) ** (-MONTHLY_DURATION)))
-
-
-    print(round(MONTHLY_PAYMENT, 2))
